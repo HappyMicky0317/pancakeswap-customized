80214a7365725229458427fca6a9d93c517ec06c
Brian0317 <onedev975@gmail.com>
2023-02-24 21:24:47 +0300

    w

--- src/components/SearchModal/CurrencySearchModal.tsx
+++ src/components/SearchModal/CurrencySearchModal.tsx
@@ -51,7 +51,7 @@ export default function CurrencySearchModal({
   showCommonBases = false,
 }: CurrencySearchModalProps) {
   const [modalView, setModalView] = useState<CurrencyModalView>(CurrencyModalView.search)
-
+// dfdfdfdfd
   const handleCurrencySelect = useCallback(
     (currency: Currency) => {
       onDismiss()
